470ec14fb0fecd3fef41477c47909c1fccfd477f
Zsofia Koma <komazsofi@gmail.com>
2018-01-15 17:23:39 +0100

    stabilize

--- lastools.bat
+++ lastools.bat
@@ -1,3 +1,4 @@
-set filename=D:\Wetland\GEOBIA\Data\AHN2\12en1_selsel
+set filename=D:\Wetland\GEOBIA\Data\AHN2\12en1_sel2
 
 C:\LAStools\bin\lasground_new -i %filename%.las -o %filename%_ground.las
+::C:\LAStools\bin\lasheight -i %filename%_ground.las -o %filename%_ground_height.las -oparse z
