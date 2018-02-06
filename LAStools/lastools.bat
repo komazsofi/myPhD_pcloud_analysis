1639058edc50916a8a0f86e631f7ad943622ae6d
Zsofia Koma <komazsofi@gmail.com>
2018-02-06 23:59:41 +0100

    vertical distr

--- LAStools/lastools.bat
+++ LAStools/lastools.bat
@@ -1,4 +1,4 @@
-set filename=D:\NAEM\Data\ALS_AHN2\SelStudyArea2_sel1
+set filename=D:\NAEM\Data\ALS_AHN2\SelStudyArea2_AHN1
 
 C:\LAStools\bin\lasground_new -i %filename%.las -o %filename%_ground.las -step 1
 C:\LAStools\bin\lasheight -i %filename%_ground.las -o %filename%_ground_height.las
--- R_pcloudAnalysis/LidRTest.R
+++ R_pcloudAnalysis/LidRTest.R
@@ -1,8 +1,14 @@
 library(lidR)
 
-filename="D://NAEM//Data//ALS_AHN2//SelStudyArea2_sel1_ground.las"
+filename="D://NAEM//Data//ALS_AHN2//SelStudyArea2_sel1_ground_height.las"
 
 las=readLAS(filename)
+
+chm2 = grid_canopy(las, 0.2, subcircle = 0.2)
+plot(chm2)
+
+entropy = las %>% grid_metrics(entropy(Z))
+plot(entropy)
 
 chm = grid_canopy(las, res = 0.5, subcircle = 0.2, na.fill = "knnidw", k = 4)
 chm = as.raster(chm)
@@ -20,3 +26,17 @@ contour = rasterToPolygons(crowns, dissolve = TRUE)
 
 plot(chm, col = height.colors(50))
 plot(contour, add = T)
+
+z=las@data$Z
+nofret=matrix(data=NA, nrow=length(seq(min(z), max(z), by=2)), ncol=1)
+h=seq(min(z), max(z)-1, by=1)
+
+for (i in seq(min(z), max(z), by=1)) { nofret[i]=length(which((z>i) & (z<i+2)))/length(z)*100}
+
+library(ggplot2)
+
+theme_set(theme_gray(base_size = 18))
+
+qplot(h,nofret)+
+  geom_line(linetype="dashed", color="blue", size=1.2)+
+  geom_point(color="red", size=3)+ coord_flip()
