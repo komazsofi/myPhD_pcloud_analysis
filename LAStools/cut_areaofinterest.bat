8508ee74d4b1de5daa974364968a74ede9f3ef8c
Zsofia Koma <komazsofi@gmail.com>
2018-02-06 17:16:21 +0100

    R segmentation

--- LAStools/cut_areaofinterest.bat
+++ LAStools/cut_areaofinterest.bat
@@ -1,9 +1,9 @@
-set lasfile1=D:\NAEM\Data\ALS_AHN2\g32hz1
-set lasfile2=D:\NAEM\Data\ALS_AHN2\u32hz1
+set lasfile1=D:\NAEM\Data\ALS_AHN2\AHN1_g40bn2
+set lasfile2=D:\NAEM\Data\ALS_AHN2\AHN1_40bn2
 
-set shpfile=D:\NAEM\Data\Field\ConfCentrum.shp
+set shpfile=D:\NAEM\Data\Field\SelStudyArea2.shp
 
-set outfile=D:\NAEM\Data\ALS_AHN2\ConfCentrum
+set outfile=D:\NAEM\Data\ALS_AHN2\SelStudyArea2_AHN1
 
 C:\LAStools\bin\lasclip -i %lasfile1%.laz -poly %shpfile% -v -o %lasfile1%_1.las
 C:\LAStools\bin\lasclip -i %lasfile2%.laz -poly %shpfile% -v -o %lasfile2%_1.las
--- R_pcloudAnalysis/LidRTest.R
+++ R_pcloudAnalysis/LidRTest.R
@@ -1,7 +1,22 @@
 library(lidR)
 
-filename="D://NAEM//Data//ALS_AHN2//TestData_ConfCentrum.las"
+filename="D://NAEM//Data//ALS_AHN2//SelStudyArea2_sel1_ground.las"
 
 las=readLAS(filename)
 
-grid_metrics(las, max(Z), 2) %>% plot
+chm = grid_canopy(las, res = 0.5, subcircle = 0.2, na.fill = "knnidw", k = 4)
+chm = as.raster(chm)
+kernel = matrix(1,3,3)
+chm = raster::focal(chm, w = kernel, fun = mean)
+chm = raster::focal(chm, w = kernel, fun = mean)
+
+crowns = lastrees(las, "watershed", chm, th = 2, extra = TRUE)
+
+tree = lasfilter(las, !is.na(treeID))
+plot(tree, color = "treeID", colorPalette = pastel.colors(100), size = 3)
+
+library(raster)
+contour = rasterToPolygons(crowns, dissolve = TRUE)
+
+plot(chm, col = height.colors(50))
+plot(contour, add = T)
