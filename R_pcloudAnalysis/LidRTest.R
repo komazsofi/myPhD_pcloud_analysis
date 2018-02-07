6c0a2f6d7f8f4cfd140a085ceccfe08cd274d441
Zsofia Koma <komazsofi@gmail.com>
2018-02-07 01:00:34 +0100

    small pdens anal

--- R_pcloudAnalysis/LidRTest.R
+++ R_pcloudAnalysis/LidRTest.R
@@ -1,11 +1,28 @@
 library(lidR)
+library(ggplot2)
 
-filename="D://NAEM//Data//ALS_AHN2//SelStudyArea2_sel1_ground_height.las"
+theme_set(theme_gray(base_size = 18))
 
-las=readLAS(filename)
+filename1="D://NAEM//Data//ALS_AHN2//SelStudyArea2_sel1_ground_height.las"
+filename2="D://NAEM//Data//ALS_AHN2//SelStudyArea2_sel1_ground_height_sub1.las"
+filename3="D://NAEM//Data//ALS_AHN2//SelStudyArea2_sel1_ground_height_sub2.las"
+filename4="D://NAEM//Data//ALS_AHN2//SelStudyArea2_sel1_ground_height_sub3.las"
 
-chm2 = grid_canopy(las, 0.2, subcircle = 0.2)
+las1=readLAS(filename1)
+las2=readLAS(filename2)
+las3=readLAS(filename3)
+las4=readLAS(filename4)
+
+chm1 = grid_canopy(las1, 0.2, subcircle = 0.2)
+plot(chm1)
+chm2 = grid_canopy(las2, 0.2, subcircle = 0.2)
 plot(chm2)
+chm3 = grid_canopy(las3, 0.2, subcircle = 0.2)
+plot(chm3)
+chm4 = grid_canopy(las4, 0.2, subcircle = 0.2)
+plot(chm4)
+
+boxplot(as.vector(chm1$Z),as.vector(chm2$Z),as.vector(chm3$Z),as.vector(chm4$Z),names=c("10 pt/m2","7 pt/m2","4 pt/m2","2 pt/m2"),col="blue")
 
 entropy = las %>% grid_metrics(entropy(Z))
 plot(entropy)
@@ -32,10 +49,6 @@ nofret=matrix(data=NA, nrow=length(seq(min(z), max(z), by=2)), ncol=1)
 h=seq(min(z), max(z)-1, by=1)
 
 for (i in seq(min(z), max(z), by=1)) { nofret[i]=length(which((z>i) & (z<i+2)))/length(z)*100}
-
-library(ggplot2)
-
-theme_set(theme_gray(base_size = 18))
 
 qplot(h,nofret)+
   geom_line(linetype="dashed", color="blue", size=1.2)+
